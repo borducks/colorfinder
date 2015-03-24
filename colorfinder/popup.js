0a4b8125064bec22cbe204e83e8874988676c18f
Borducks <paul.bordeaux@answers.com>
2015-03-24 10:36:50 -0500

    add term highlighting to result text

--- colorfinder/popup.js
+++ colorfinder/popup.js
@@ -13,31 +13,48 @@ term.addEventListener('keyup', function(e) {
     }
     // add new ones
     for (var i = 0; i < colorkeys.length; i++) {
-        if (colorkeys[i].indexOf(e.target.value) > -1 || colordata[colorkeys[i]].indexOf(e.target.value) > -1) {
-            var l = document.createElement("DIV");
-            var k = document.createElement("SPAN");
-            k.className = "key";
-            var v = document.createElement("SPAN");
-            var s = document.createElement("SPAN");
-            s.className = "swatch";
-
-            // set current color
-            if (colordata[colorkeys[i]].indexOf("#") === 0) {
-                s.style.backgroundColor = colordata[colorkeys[i]];
-            } else if (colordata[colordata[colorkeys[i]]] !== undefined && colordata[colordata[colorkeys[i]]].indexOf("#") === 0) {
-                s.style.backgroundColor = colordata[colordata[colorkeys[i]]];
-            }
-
-            k.appendChild(document.createTextNode(colorkeys[i]));
-            v.appendChild(document.createTextNode(colordata[colorkeys[i]]));
-            l.appendChild(s);
-            l.appendChild(k);
-            l.appendChild(v);
-            l.className += "color_result";
-            result.appendChild(l);
+        var term = e.target.value;
+        var key;
+        var val;
+        if (colorkeys[i].indexOf(term) > -1 || colordata[colorkeys[i]].indexOf(term) > -1) {
+            key = colorkeys[i];
+            val = colordata[colorkeys[i]];
+            createResultItem(key, val, term);
         }
     }
 });
+
+function createResultItem(key, value, term) {
+    var l = document.createElement("DIV");
+    var k = document.createElement("SPAN");
+    k.className = "key";
+    var v = document.createElement("SPAN");
+    var s = document.createElement("SPAN");
+    s.className = "swatch";
+
+    var key_text = key;
+    var val_text = value;
+
+
+    // set current color
+    if (value.indexOf("#") === 0) {
+        s.style.backgroundColor = colordata[key];
+    } else if (colordata[value] !== undefined && colordata[value].indexOf("#") === 0) {
+        s.style.backgroundColor = colordata[value];
+    }
+
+    // highlight found substring in display text
+    key_text = key_text.replace(term, "<b>" + term + "</b>");
+    val_text = val_text.replace(term, "<b>" + term + "</b>");
+
+    k.innerHTML = key_text;
+    v.innerHTML = val_text;
+    l.appendChild(s);
+    l.appendChild(k);
+    l.appendChild(v);
+    l.className += "color_result";
+    result.appendChild(l);
+}
 
 // oad color data from local storage
 getColorData().then(function(response) {
